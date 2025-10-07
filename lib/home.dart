968b4ba4f42be167bb89fceae5b339ec4517dbff
Aditya <adeeteya@gmail.com>
2025-10-07 05:33:55 -0500

    ✨ added html tags support

--- lib/home.dart
+++ lib/home.dart
@@ -7,7 +7,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:markdown_editor/device_preference_notifier.dart';
 import 'package:markdown_editor/l10n/generated/app_localizations.dart';
-import 'package:markdown_editor/widgets/MarkdownBody/custom_image_config.dart';
+import 'package:markdown_editor/widgets/MarkdownBody/custom_text_node.dart';
 import 'package:markdown_editor/widgets/MarkdownTextInput/markdown_text_input.dart';
 import 'package:markdown_widget/markdown_widget.dart';
 
@@ -226,7 +226,12 @@ class _HomeState extends State<Home> {
             padding: const EdgeInsets.all(8),
             child: MarkdownBlock(
               data: _inputText,
-              config: config.copy(configs: [CustomImgConfig()]),
+              generator: MarkdownGenerator(
+                textGenerator: (node, config, visitor) =>
+                    CustomTextNode(node.textContent, config, visitor),
+                richTextBuilder: Text.rich,
+              ),
+              config: config,
             ),
           ),
         ),
