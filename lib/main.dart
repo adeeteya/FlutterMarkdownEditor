00af6775b78f7bae3c2091f757bf97fbfa5f2e2c
Aditya <adeeteya@gmail.com>
2026-04-13 06:28:39 -0700

    ✨ Integrate dynamic color support for improved theme customization

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -29,28 +30,34 @@ class MarkdownEditorApp extends StatelessWidget {
     return ValueListenableBuilder<DevicePreferences>(
       valueListenable: devicePreferenceNotifier,
       builder: (context, devicePreference, _) {
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          title: 'Markdown Editor',
-          themeMode: devicePreference.isDarkMode
-              ? ThemeMode.dark
-              : ThemeMode.light,
-          theme: ThemeData(
-            useMaterial3: true,
-            textTheme: GoogleFonts.notoSansTextTheme(),
-            colorSchemeSeed: Colors.indigo,
-          ),
-          darkTheme: ThemeData(
-            useMaterial3: true,
-            textTheme: GoogleFonts.notoSansTextTheme(
-              ThemeData(brightness: Brightness.dark).textTheme,
-            ),
-            brightness: Brightness.dark,
-            colorSchemeSeed: Colors.indigo,
-          ),
-          localizationsDelegates: AppLocalizations.localizationsDelegates,
-          supportedLocales: AppLocalizations.supportedLocales,
-          home: Home(devicePreferenceNotifier: devicePreferenceNotifier),
+        return DynamicColorBuilder(
+          builder: (lightDynamic, darkDynamic) {
+            return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              title: 'Markdown Editor',
+              themeMode: devicePreference.isDarkMode
+                  ? ThemeMode.dark
+                  : ThemeMode.light,
+              theme: ThemeData(
+                useMaterial3: true,
+                textTheme: GoogleFonts.notoSansTextTheme(),
+                colorScheme: lightDynamic,
+                colorSchemeSeed: lightDynamic == null ? Colors.indigo : null,
+              ),
+              darkTheme: ThemeData(
+                useMaterial3: true,
+                textTheme: GoogleFonts.notoSansTextTheme(
+                  ThemeData(brightness: Brightness.dark).textTheme,
+                ),
+                brightness: Brightness.dark,
+                colorScheme: darkDynamic,
+                colorSchemeSeed: darkDynamic == null ? Colors.indigo : null,
+              ),
+              localizationsDelegates: AppLocalizations.localizationsDelegates,
+              supportedLocales: AppLocalizations.supportedLocales,
+              home: Home(devicePreferenceNotifier: devicePreferenceNotifier),
+            );
+          },
         );
       },
     );
--- linux/flutter/generated_plugin_registrant.cc
+++ linux/flutter/generated_plugin_registrant.cc
@@ -6,10 +6,14 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <dynamic_color/dynamic_color_plugin.h>
 #include <printing/printing_plugin.h>
 #include <url_launcher_linux/url_launcher_plugin.h>
 
 void fl_register_plugins(FlPluginRegistry* registry) {
+  g_autoptr(FlPluginRegistrar) dynamic_color_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "DynamicColorPlugin");
+  dynamic_color_plugin_register_with_registrar(dynamic_color_registrar);
   g_autoptr(FlPluginRegistrar) printing_registrar =
       fl_plugin_registry_get_registrar_for_plugin(registry, "PrintingPlugin");
   printing_plugin_register_with_registrar(printing_registrar);
